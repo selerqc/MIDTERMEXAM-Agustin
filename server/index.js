ab5dcc2c9f2dbf47f571884c6f921257140982bf
selerqc <agustincharles34@gmail.com>
2025-04-11 10:28:58 +0800

    Update Navbar and Viewbooks styles, clean up books.json, and adjust server index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const fs = require("fs");
-const e = require("express");
+
 app.use(cors());
 app.use(express.json());
 
